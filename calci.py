93b54758d41800f1d44624047634f673817144f5
ananya s <ananyas.bsc22@rvu.edu.in>
2023-07-18 00:17:20 +0530

    initial commit

---
+++ calci.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    total = 0
+    total=a+b
+    return total
+def substract(c,d):    
+    total=c-d
+    return total
+a = add(2,3)
+s=substract(10,6)
+total=a-s
+print("Total:", total)
