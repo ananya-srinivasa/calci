dc616398019f4395b72b8b5183a6c1a34bcd46d4
ananya s <ananyas.bsc22@rvu.edu.in>
2023-07-21 12:56:34 +0530

    unit test for divide

--- test_unit.py
+++ test_unit.py
@@ -2,6 +2,7 @@ import unittest
 from calci import add
 from calci import substract
 from calci import multiply
+from calci import divide
 
 class AddTestCase(unittest.TestCase):
     def test_add(self):
@@ -12,7 +13,11 @@ class AddTestCase(unittest.TestCase):
         self.assertEqual(r,-3,"incorrect ")
     def test_multi(self):
         result=multiply(3,6)
-        self.assertEqual(result,18,"incorrect ")        
+        self.assertEqual(result,18,"incorrect ")       
+    def test_divide(self):
+        result=divide(6,3)
+        self.assertEqual(result,2,"incorrect ")
+             
 
 if __name__ == '__main__':
     unittest.main()
